28360b1bc63faf10bf685d455934bf3a75e10560
John Sime <johnc.sime@gmail.com>
2018-10-02 09:14:48 +0000

    automatically backed up by learn

--- cartoon_collections.rb
+++ cartoon_collections.rb
@@ -8,8 +8,10 @@ def summon_captain_planet(planeteer_calls)
   planeteer_calls.map { |call| call.capitalize + "!" }
 end
 
-def long_planteer_calls(planeteer_calls)
-  planeteer_calls.any? { |call| calls_long.length > 4 }
+def long_planeteer_calls(calls)
+  calls.any? do |call|
+    call.length > 4
+  end
 end
 
 def find_the_cheese(potentially_cheesy_items)
